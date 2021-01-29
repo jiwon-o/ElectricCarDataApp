e7e04996b1a02e93578b4c20bae7a4f4c4d1d9b7
오지원 <64193469+ojw4184@users.noreply.github.com>
2021-01-29 17:07:37 +0900

    data fragment에 그래프 삽입
    2개의 데이터를 한 fragment에 추가하느라 애먹음

---
+++ app/src/main/java/com/example/electriccardataapp/MyMarkerView.java
@@ -0,0 +1,48 @@
+package com.example.electriccardataapp;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.components.MarkerView;
+import com.github.mikephil.charting.data.CandleEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.utils.MPPointF;
+import com.github.mikephil.charting.utils.Utils;
+
+public class MyMarkerView extends MarkerView {
+    /**
+     * Constructor. Sets up the MarkerView with a custom layout resource.
+     *
+     * @param context
+     * @param layoutResource the layout resource to use for the MarkerView
+     */
+
+    private TextView tvContent;
+    public MyMarkerView(Context context, int layoutResource) {
+        super(context, layoutResource);
+
+        tvContent = (TextView) findViewById(R.id.tv_content);
+    }
+
+    @Override
+    public void refreshContent(Entry e, Highlight highlight) {
+
+        if (e instanceof CandleEntry) {
+
+            CandleEntry ce = (CandleEntry) e;
+
+            tvContent.setText("" + Utils.formatNumber(ce.getHigh(), 0, true) + "km");
+        } else {
+
+            tvContent.setText("" + Utils.formatNumber(e.getY(), 0, true) + "km");
+        }
+
+        super.refreshContent(e, highlight);
+    }
+
+    @Override
+    public MPPointF getOffset() {
+        return new MPPointF(-(getWidth() / 2), -getHeight() + 2);
+    }
+}
