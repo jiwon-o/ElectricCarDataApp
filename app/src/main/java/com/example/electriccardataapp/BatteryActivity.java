6adeed837c9d8592332a227854e9827242e5d6a2
오지원 <64193469+ojw4184@users.noreply.github.com>
2021-02-03 17:13:37 +0900

    배터리 Activity (Pie chart) 추가

--- app/src/main/java/com/example/electriccardataapp/BatteryActivity.java
+++ app/src/main/java/com/example/electriccardataapp/BatteryActivity.java
@@ -1,5 +1,6 @@
 package com.example.electriccardataapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -7,9 +8,20 @@ import android.widget.ImageView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+
+import java.util.ArrayList;
+
 public class BatteryActivity extends AppCompatActivity {
 
-   ImageView backImage;
+    ImageView backImage;
+    private PieChart chart;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,5 +36,73 @@ public class BatteryActivity extends AppCompatActivity {
             }
         });
 
+        chart = findViewById(R.id.chart_pie);
+        chart.setUsePercentValues(true);
+        chart.getDescription().setEnabled(false);
+        chart.setExtraOffsets(5, 10, 5, 5);
+
+        chart.setDragDecelerationFrictionCoef(0.95f);
+
+        chart.setDrawHoleEnabled(true);
+        chart.setHoleColor(Color.BLACK);
+
+        chart.setTransparentCircleColor(Color.WHITE);
+        chart.setTransparentCircleAlpha(0);
+
+        chart.setHoleRadius(58f);
+        chart.setTransparentCircleRadius(65f);
+
+        chart.setRotationAngle(0);
+        // enable rotation of the chart by touch
+        chart.setRotationEnabled(true);
+        chart.setHighlightPerTapEnabled(true);
+
+        chart.animateY(1400, Easing.EaseInOutQuad);
+        // chart.spin(2000, 0, 360);
+
+        chart.getLegend().setEnabled(false);
+
+
+
+        // entry label styling
+        chart.setEntryLabelColor(Color.WHITE);
+        chart.setEntryLabelTextSize(11f);
+
+        setData(4, 25);
+    }
+
+    private void setData(int count, float range) {
+        ArrayList<PieEntry> entries = new ArrayList<>();
+
+        // NOTE: The order of the entries when being added to the entries array determines their position around the center of
+        // the chart.
+        entries.add(new PieEntry((float) ((Math.random() * range) + range / 5), "Driving Meter"));
+        entries.add(new PieEntry((float) ((Math.random() * range) + range / 5), "Air Condition"));
+        entries.add(new PieEntry((float) ((Math.random() * range) + range / 5), "Battery Field"));
+        entries.add(new PieEntry((float) ((Math.random() * range) + range / 5), "Battery Care"));
+
+        PieDataSet dataSet = new PieDataSet(entries, "");
+
+        dataSet.setDrawIcons(false);
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+
+        // add a lot of colors
+        dataSet.setColors(Color.rgb(140, 26, 28),
+                Color.rgb(143, 50, 51),
+                Color.rgb(143, 77, 77),
+                Color.rgb(140, 101, 102));
+
+
+        PieData data = new PieData(dataSet);
+        data.setValueFormatter(new PercentFormatter());
+        data.setValueTextSize(16f);
+        data.setValueTextColor(Color.WHITE);
+        chart.setData(data);
+
+        // undo all highlights
+        chart.highlightValues(null);
+
+        chart.invalidate();
     }
 }
