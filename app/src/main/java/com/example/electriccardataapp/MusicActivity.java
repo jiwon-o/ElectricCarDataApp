198618dfb39de5eb81f93d7beb97a47bc57129e3
오지원 <64193469+ojw4184@users.noreply.github.com>
2021-02-04 18:33:55 +0900

    Music Activity 구현 완료

--- app/src/main/java/com/example/electriccardataapp/MusicActivity.java
+++ app/src/main/java/com/example/electriccardataapp/MusicActivity.java
@@ -1,17 +1,29 @@
 package com.example.electriccardataapp;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
 public class MusicActivity extends AppCompatActivity {
 
-    Toolbar toolbar;
     ImageView backImage;
+
+    ImageView playImage;
+
+    boolean i = true;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,6 +38,59 @@ public class MusicActivity extends AppCompatActivity {
             }
         });
 
+        ListView listview ;
+        ListViewAdapter adapter;
+
+        adapter = new ListViewAdapter();
+
+        listview = (ListView) findViewById(R.id.lv_music);
+        listview.setAdapter(adapter);
+
+        adapter.addItem("Schubert : impromptu 90-2", "28:07");
+        adapter.addItem("Piano Sonata no.8 - II", "03:04");
+        adapter.addItem("Moonlight Sonata", "05:25");
+        adapter.addItem("Violin Concerto No. 1 in G Minor", "06:36");
+        adapter.addItem("Swan Lake, Ballet Suite, Op. 20", "07:49");
+        adapter.addItem("Violin Romance No.2 In F Major OP 50", "08:64");
+        adapter.addItem("Serenade Op.15 - N.7", "03:00");
+        adapter.addItem("Symphony No. 101 in D Major", "05:25");
+        adapter.addItem("Chopin: Nocturne Opus 9 No. 2", "03:21");
+        adapter.addItem("Andrew Lloyd Webber: Memory", "28:07");
+        adapter.addItem("Variations On The Cannon", "03:02");
+
+
+        // 위에서 생성한 listview에 클릭 이벤트 핸들러 정의.
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                // get item
+                ListViewItem item = (ListViewItem) parent.getItemAtPosition(position) ;
+
+                String titleStr = item.getTitle() ;
+                String descStr = item.getDesc() ;
+
+                // TODO : use item data.
+            }
+        });
+
+        playImage = findViewById(R.id.img_play);
+
+        playButton(playImage);
+    }
+
+    private void playButton(ImageView playImage) {
+        playImage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (i == true){
+                    playImage.setImageResource(R.drawable.ic_play_pause);
+                    i = false;
+                }else {
+                    playImage.setImageResource(R.drawable.ic_play_red);
+                    i = true;
+                }
+            }
+        });
     }
 
 
